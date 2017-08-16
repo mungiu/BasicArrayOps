5108707168a344fe009507c7a629c3db5a17820c
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-08-16 10:47:21 +0200

    Add project files.

---
+++ BasicArrayOps/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicArrayOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string monday = "Monday";
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            string tuesday = daysOfWeek[1];
+            Console.WriteLine(tuesday);
+        }
+    }
+}
